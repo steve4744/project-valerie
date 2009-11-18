773b8de30a56c5176a560c0c384172014bd3de85
gr.hellmaster@gmx.de <gr.hellmaster@gmx.de@87702144-8a56-11de-a519-b78fc8232508>
2009-11-18 22:06:59 +0000

    valerie as mediacenter extension

---
+++ ValerieMediaCenter/DMC_Movies.py
@@ -0,0 +1,275 @@
+from enigma import eTimer, eWidget, eRect, eServiceReference, iServiceInformation, iPlayableService, ePicLoad
+from Screens.Screen import Screen
+from Screens.ServiceInfo import ServiceInfoList, ServiceInfoListEntry
+from Screens.ChoiceBox import ChoiceBox
+from Components.ActionMap import ActionMap, NumberActionMap, HelpableActionMap
+from Components.Pixmap import Pixmap, MovingPixmap
+from Components.Label import Label
+from Components.Button import Button
+
+from Components.Sources.List import List
+from Screens.MessageBox import MessageBox
+from Screens.HelpMenu import HelpableScreen
+
+from Components.ServicePosition import ServicePositionGauge
+from Components.ServiceEventTracker import ServiceEventTracker, InfoBarBase
+
+from Components.ConfigList import ConfigList, ConfigListScreen
+from Components.config import *
+
+from Tools.Directories import resolveFilename, fileExists, pathExists, createDir, SCOPE_MEDIA, SCOPE_SKIN_IMAGE
+from Components.FileList import FileList
+from Components.AVSwitch import AVSwitch
+from Screens.InfoBar import MoviePlayer
+
+from Plugins.Plugin import PluginDescriptor
+
+#from enigma import eStillPicture
+from Components.MenuList import MenuList
+
+import os
+from os import path as os_path
+
+def getAspect():
+	val = AVSwitch().getAspectRatioSetting()
+	return val/2
+
+try:
+	from enigma import eStillPicture
+except Exception, e:
+	print "No build in Stillpicture support!"
+	hasBuildInStillpicture = False
+
+def finishStillPicture():
+	if not hasBuildInStillpicture:
+		os.system("killall showiframe")
+		
+
+def showStillpicture(picture):
+	if not os.path.exists(picture):
+		picture="/boot/bootlogo.mvi"
+	if hasBuildInStillpicture:
+		eStillPicture.getInstance().showSinglePic(picture)
+	else:
+		finishStillPicture()
+		os.system("/usr/bin/showiframe " + picture + " &")
+
+#------------------------------------------------------------------------------------------
+
+class DMC_Movies(Screen, HelpableScreen, InfoBarBase):
+
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		InfoBarBase.__init__(self)
+		HelpableScreen.__init__(self)
+
+		self.oldService = self.session.nav.getCurrentlyPlayingServiceReference()
+		self.session.nav.stopService()		
+
+		self.isVisible = True
+		self.moviedb = {}
+		self.genreFilter = ""
+		self.oldService = self.session.nav.getCurrentlyPlayingServiceReference()
+		list = []
+		self["title"] = Label()
+		self["otitle"] = Label()
+		self["tag"] = Label()
+		self["poster"] = Pixmap()
+		self["shortDescription"] = Label()
+		self["director"] = Label()
+		self["writer"] = Label()
+		self["genre"] = Label()
+		self["year"] = Label()
+		self["runtime"] = Label()
+
+		for i in range(10):
+			stars = "star" + str(i)
+			print stars
+			self[stars] = Pixmap()
+
+
+		
+		self["actions"] = HelpableActionMap(self, "MC_AudioPlayerActions", 
+			{
+				"ok": (self.KeyOk, "Play selected file"),
+				"cancel": (self.Exit, "Exit Audio Player"),
+				"left": (self.leftUp, "List Top"),
+				"right": (self.rightDown, "List Bottom"),
+				"up": (self.up, "List up"),
+				"down": (self.down, "List down"),
+				"blue": (self.KeyGenres, "Genres"),
+			}, -2)
+		self.MenuList = MenuList(list)
+		self["listview"] = self.MenuList
+		self.loadMoviesDB()
+		
+
+	def loadMoviesDB(self):
+		filter = []
+		list = []
+		filter.append("Tag")
+		filter.append("Plot")
+		filter.append("Directors")
+		filter.append("Writers")
+		filter.append("Genres")
+		filter.append("Year")
+		filter.append("Runtime")
+		filter.append("Popularity")
+		filter.append("ImdbId")
+		filter.append("Title")
+		filter.append("LocalTitle")
+		filter.append("Path")
+		
+		try:
+			db = open("/hdd/valerie/moviedb.txt").read()[:-1]
+			movies = db.split("\n----END----\n")
+			
+
+			for movie in movies:
+				movie = movie.split("---BEGIN---\n")
+				if len(movie) == 2: 
+					d = {} 
+					lines = movie[1].split('\n')
+					for line in lines: 
+						#print "Line: ", line
+						if ":" in line: 
+						    key, text = (s.strip() for s in line.split(":", 1)) 
+
+						if key in filter: 
+							d[key] = text
+
+					#print d
+					if self.genreFilter != "" and d["Genres"] != "" and not self.genreFilter in d["Genres"]:
+						print "skipping ", d["LocalTitle"]
+						continue
+					self.moviedb[d["ImdbId"]] = d
+					if d["LocalTitle"] != "":
+						print "adding ", d["LocalTitle"]
+						list.append((d["LocalTitle"], d["ImdbId"], "menu_globalsettings", "45"))
+					else:
+						print "adding ", d["Title"]
+						list.append((d["Title"], d["ImdbId"], "menu_globalsettings", "45"))
+			
+		except OSError, e: 
+			print "OSError: ", e
+
+		list.sort()
+		self.MenuList.setList(list)
+		#self["listview"].setCurrent(1)
+		#self.refresh()
+
+	def getAvailGenres(self):
+		list = []
+		glist = []
+		list.append((_("All"), "all"))
+		try:
+			db = open("/hdd/valerie/moviedb.txt").read()[:-1]
+			movies = db.split("\n----END----\n")
+			
+
+			for movie in movies:
+				movie = movie.split("---BEGIN---\n")
+				if len(movie) == 2: 
+					d = {} 
+					lines = movie[1].split('\n')
+					for line in lines: 
+					#print "Line: ", line
+						if ":" in line: 
+						    key, text = (s.strip() for s in line.split(":", 1)) 
+
+						if "Genres" in key : 
+							gens = text.split(';')
+							for gen in gens:
+								gen=gen.lstrip().rstrip()
+								if not gen in glist:
+									glist.append(gen)
+									list.append((_(gen), gen))
+						
+			
+		except OSError, e: 
+			print "OSError: ", e
+
+		list.sort()
+		return list
+		
+	def refresh(self):
+		selection = self["listview"].getCurrent()
+		if selection is not None:
+			showStillpicture("/hdd/valerie/media/tt" + selection[1] + "_backdrop.m1v")
+			self["poster"].instance.setPixmapFromFile("/hdd/valerie/media/tt" + selection[1] + "_poster.png")
+			self["title"].setText(selection[0])
+			self["otitle"].setText(self.moviedb[selection[1]]["Title"])
+			self["tag"].setText(self.moviedb[selection[1]]["Tag"])
+			self["shortDescription"].setText(self.moviedb[selection[1]]["Plot"])
+			self["director"].setText(self.moviedb[selection[1]]["Directors"])
+			self["writer"].setText(self.moviedb[selection[1]]["Writers"])
+			self["genre"].setText(self.moviedb[selection[1]]["Genres"])
+			self["year"].setText(self.moviedb[selection[1]]["Year"])
+			self["runtime"].setText(self.moviedb[selection[1]]["Runtime"])
+			
+			for i in range(int(self.moviedb[selection[1]]["Popularity"])):
+				self["star" + str(i)].instance.setPixmapFromFile("/usr/lib/enigma2/python/Plugins/Extensions/MediaCenter/skins/defaultHD/images/Valerie_Star.png")
+
+			for i in range(10 - int(self.moviedb[selection[1]]["Popularity"])):
+				self["star" + str(9 - i)].instance.setPixmapFromFile("/usr/lib/enigma2/python/Plugins/Extensions/MediaCenter/skins/defaultHD/images/Valerie_NoStar.png")
+
+	def up(self):
+		self["listview"].up()
+		self.refresh()
+		
+
+	def down(self):
+		self["listview"].down()
+		self.refresh()
+		
+	def leftUp(self):
+		self["listview"].pageUp()
+		self.refresh()
+		
+	def rightDown(self):
+		self["listview"].pageDown()
+		self.refresh()
+
+
+	def KeyOk(self):
+		if self.isVisible == False:
+			self.visibility()
+			return
+		
+		finishStillPicture()
+		
+		selection = self["listview"].getCurrent()
+		if selection is not None:
+			self.session.openWithCallback(self.leaveMoviePlayer, MoviePlayer, eServiceReference("4097:0:1:0:0:0:0:0:0:0:" + self.moviedb[selection[1]]["Path"]))
+		
+
+	def leaveMoviePlayer(self): 
+		self.session.nav.playService(None) 
+		selection = self["listview"].getCurrent()
+		if selection is not None:
+			showStillpicture("/hdd/valerie/media/tt" + selection[1] + "_backdrop.m1v")
+
+	def KeyGenres(self):
+		menu=self.getAvailGenres()
+		self.session.openWithCallback(self.genresCallback, ChoiceBox, title="Select Genre", list=menu)
+
+	def genresCallback(self, choice):
+		if choice is None:
+			return
+
+		if choice[1] == "all":
+			self.genreFilter = ""
+		else:
+			self.genreFilter = choice[1]
+		self.loadMoviesDB()
+
+	def Exit(self):
+		if self.isVisible == False:
+			self.visibility()
+			return
+		
+		finishStillPicture()
+
+		self.close()
+
+#------------------------------------------------------------------------------------------
---
+++ ValerieMediaCenter/MC_RSSReader.py
@@ -0,0 +1,41 @@
+from enigma import eTimer, eWidget, eRect, eServiceReference, iServiceInformation
+from Screens.Screen import Screen
+from Screens.ServiceInfo import ServiceInfoList, ServiceInfoListEntry
+from Components.ActionMap import ActionMap, NumberActionMap
+from Components.Pixmap import Pixmap, MovingPixmap
+from Components.Label import Label
+from Components.Button import Button
+
+from Screens.MessageBox import MessageBox
+
+from Components.ConfigList import ConfigList
+from Components.config import *
+
+from Tools.Directories import resolveFilename, fileExists, pathExists, createDir, SCOPE_MEDIA, SCOPE_SKIN_IMAGE
+from Components.FileList import FileList
+from Components.AVSwitch import AVSwitch
+from Screens.InfoBar import MoviePlayer
+from Plugins.Plugin import PluginDescriptor
+
+import os
+
+def getAspect():
+	val = AVSwitch().getAspectRatioSetting()
+	return val/2
+
+#------------------------------------------------------------------------------------------
+
+class MC_RSSReader(Screen):
+	def __init__(self, session):
+		Screen.__init__(self, session)
+		
+		self["actions"] = ActionMap(["OkCancelActions", "DirectionActions", "ColorActions", "MovieSelectionActions", "MenuActions", "MoviePlayerActions", "ChannelSelectBaseActions"],
+		{
+			"ok": self.Exit,
+			"cancel": self.Exit
+		}, -1)
+		
+		self.aspect = getAspect()
+
+	def Exit(self):
+		self.close()
