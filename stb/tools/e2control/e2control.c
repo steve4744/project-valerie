2351f189fc87cf609859413686a26f79b4606e31
schischu65 <schischu65@87702144-8a56-11de-a519-b78fc8232508>
2010-02-13 23:00:55 +0000

    [e2control] is now a daemon with a control interface

--- stb/tools/e2control/e2control.c
+++ stb/tools/e2control/e2control.c
@@ -1,7 +1,7 @@
 /* 
  * e2control.c
  *
- * Copyright (C) 2004 Marcus Metzler <mocm@metzlerbros.de>
+ * Copyright (C) 2010 Schischu <schischu65@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -18,10 +18,11 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-
-#include <sys/ioctl.h>
+#include <arpa/inet.h> 
 #include <stdio.h>
+#include <stdarg.h>
 #include <string.h>
+
 #include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -29,8 +30,9 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <netinet/in.h>
-#include <arpa/inet.h> 
+//#include <netinet/in.h>
+
+//#include <sys/un.h>
 
 #include <pthread.h>
 
@@ -39,6 +41,7 @@
 #define MAX_CHARS_UDP 256
 #define MAX_CHARS_TCP 1460
 
+#define CONTROL_PORT 5449
 #define UDP_PORT 5450
 #define TCP_PORT 5451
 
@@ -48,7 +51,77 @@
 #define MAGIC_REQ_FILE_MT "REQ_FILE_MT"
 #define MAGIC_REQ_CMDEXEC "REQ_CMDEXEC"
 
-void *answerUdpBroadcast(void *)
+#define CONTROL_CMD_GETLOGFILENAME 0x00
+#define CONTROL_CMD_SHUTDOWN 0x01
+
+//static int fdLogfile = -1;
+static FILE * fpLogfile = NULL;
+static char * filename = "stdout";
+
+void openLogfile()
+{
+	filename = tmpnam (NULL);
+	//fdLogfile = open(filename, O_WRONLY|O_CREAT);
+	fpLogfile = fopen(filename, "w");
+	printf ("Logging to: %s\n", filename);
+}
+
+void closeLogfile()
+{
+	fclose(fpLogfile);
+}
+
+void printThis( const char* format, ... ) {
+    va_list args;
+
+    time_t time_value = time(NULL);
+    struct tm *now = localtime(&time_value);
+    fprintf(fpLogfile, "%02d:%02d:%02d ", now->tm_hour, now->tm_min, now->tm_sec);
+
+    va_start( args, format );
+    vfprintf( fpLogfile, format, args );
+    va_end( args );
+    fflush(fpLogfile);
+}
+
+//#define DEBUG
+
+void dprintf( const char* format, ... ) {
+#ifdef DEBUG
+    va_list args;
+
+    time_t time_value = time(NULL);
+    struct tm *now = localtime(&time_value);
+    fprintf(stderr, "%02d:%02d:%02d ", now->tm_hour, now->tm_min, now->tm_sec);
+
+    va_start( args, format );
+    vfprintf( stderr, format, args );
+    va_end( args );
+#endif
+}
+
+                    /* read n bytes from a socket descriptor */ 
+                    int readsock(sockfd, buf, nbytes) 
+                    register    int            sockfd; 
+                    register    char        *buf; 
+                    register    int            nbytes; 
+                    { 
+                        int nleft, nread; 
+                        nleft = nbytes; 
+                        while (nleft > 0) { 
+                                if ((nread = read(sockfd, buf, nleft)) < 0) 
+                                    return(nread); /* error, nread < 0 */ 
+                                else if (nread == 0) 
+                                    break;    /* EOF */ 
+                                /* nread > 0. update nleft and buf pointer */ 
+                                nleft -= nread; 
+                                buf += nread; 
+                        } /* while */ 
+                        return(nbytes - nleft); 
+                    } /* readsock() */ 
+ 
+
+void *answerUdpBroadcast(void * none)
 {
 
 	struct sockaddr_in bcast;
@@ -63,15 +136,15 @@ void *answerUdpBroadcast(void *)
 	bcast.sin_family = AF_INET;
 	bcast.sin_port = htons(UDP_PORT);
 	bcast.sin_addr.s_addr = htons(INADDR_ANY);
-	printf("%s:%d\n", __FUNCTION__, __LINE__);
+	printThis("%s:%d\n", __FUNCTION__, __LINE__);
 	if(inet_pton(AF_INET, "255.255.255.255", &bcast.sin_addr) <= 0) {
-		printf("inet_pton error\n");
+		printThis("inet_pton error\n");
 		pthread_exit(NULL);
 	}
 
-	printf("%s:%d\n", __FUNCTION__, __LINE__);
+	printThis("%s:%d\n", __FUNCTION__, __LINE__);
 	if(bind(bsockfd, (struct sockaddr*)&bcast, len_bcast) <0) {
-		printf("bind error\n");
+		printThis("bind error\n");
 		pthread_exit(NULL);
 	}
 
@@ -81,34 +154,34 @@ void *answerUdpBroadcast(void *)
 		struct sockaddr_in bcast, adr_client;
 		char dgram[MAX_CHARS_UDP];
 
-		printf("%s:%d\n", __FUNCTION__, __LINE__);
+		printThis("%s:%d\n", __FUNCTION__, __LINE__);
 		len_adr_client = sizeof(adr_client);
 		if((n=recvfrom(bsockfd, dgram, sizeof(dgram), 0, (struct sockaddr*)&adr_client, (socklen_t*)&len_adr_client))<0) {
-			printf("recvfrom failed\n");
+			printThis("recvfrom failed\n");
 			continue;
 		}
 
 		dgram[n]='\0';
 
-		printf("%s:%d <- %s\n", __FUNCTION__, __LINE__, dgram);
+		printThis("%s:%d <- %s\n", __FUNCTION__, __LINE__, dgram);
 		if(!strncmp(MAGIC_SERVER_UDP_BROADCAST, dgram, strlen(MAGIC_SERVER_UDP_BROADCAST)))
 		{
 			//char* s = "MANUFACTOR=Kathrein;MODEL=UFS910;";
 			char * s = getenv("BOXSYSTEM");
-			printf("%s:%d -> \"%s\" %d\n", __FUNCTION__, __LINE__, s, strlen(s));
+			printThis("%s:%d -> \"%s\" %d\n", __FUNCTION__, __LINE__, s, strlen(s));
 			if((n=sendto(bsockfd, s, strlen(s), 0, (struct sockaddr*)&adr_client, len_adr_client))<0) {
-				printf("sendto failed\n");
+				printThis("sendto failed\n");
 				continue;
 			}
-			printf("%s:%d sento %d bytes\n", __FUNCTION__, __LINE__, n);
+			printThis("%s:%d sento %d bytes\n", __FUNCTION__, __LINE__, n);
 		}
 
-		printf("%s:%d\n", __FUNCTION__, __LINE__);		
+		printThis("%s:%d\n", __FUNCTION__, __LINE__);		
 	}
 	close(bsockfd);
 }
 
-void *tcpRequests(void *)
+void * tcpRequests(void * none)
 {
 	int fdc, fdd, len_local;
 	struct sockaddr_in local, remote;
@@ -123,11 +196,6 @@ void *tcpRequests(void *)
 	local.sin_family=AF_INET;
 	local.sin_port=htons(TCP_PORT);
 	local.sin_addr.s_addr=htonl(INADDR_ANY);
-	/*if(inet_pton(AF_INET, "255.255.255.255", &local.sin_addr) <= 0) {
-		printf("inet_pton error\n");
-		continue;
-	}*/
-	//memset(&local.sin_zero, 0, sizeof(local.sin_zero));
 
 	// bind and listen on port and ip interface
 	if (bind(fdc, (struct sockaddr*)&local, len_local) < 0) {
@@ -140,18 +208,18 @@ void *tcpRequests(void *)
 	}
 
 	while(1) {
-		printf("%s:%d\n", __FUNCTION__, __LINE__);
+		printThis("%s:%d\n", __FUNCTION__, __LINE__);
 	  	int rlen=sizeof(remote);
 		// accept connection
 		if ((fdd=accept(fdc, (struct sockaddr*)&remote, (socklen_t*)&rlen)) < 0) {
-			perror("accept error");
+			printThis("accept error\n");
 			continue;
 		}
 
-		printf("%s:%d\n", __FUNCTION__, __LINE__);
+		printThis("%s:%d\n", __FUNCTION__, __LINE__);
 		//close(fdc);
 
-		printf("Got TCP-Connection from %s\n", inet_ntoa(remote.sin_addr));
+		printThis("Got TCP-Connection from %s\n", inet_ntoa(remote.sin_addr));
 
 		//First of read what the client want
 		int slen = read(fdd, string, strlen(MAGIC_REQ_PROTOYPE));
@@ -166,18 +234,18 @@ void *tcpRequests(void *)
 
 			read(fdd, &lenArray, 2);
 
-			printf("%d %d\n", lenArray[0], lenArray[1]);
+			printThis("%d %d\n", lenArray[0], lenArray[1]);
 
 			lenCmd = lenArray[0] + (lenArray[1] << 8);
 
-			printf("%d\n", lenCmd);
+			printThis("%d\n", lenCmd);
 
 			cmd = (char*)malloc(lenCmd+1);
 
 			read(fdd, cmd, lenCmd);
 			cmd[lenCmd] = '\0';
 
-			printf("CMD: %s\n", cmd);
+			printThis("CMD: %s\n", cmd);
 			FILE * output = popen(cmd, "r");
 			
 			unsigned char bSendBuffer[MAX_CHARS_TCP];
@@ -194,12 +262,12 @@ void *tcpRequests(void *)
 					int iLineLength = strlen(p_bLineBuffer);
 					short sLineLength = iLineLength & 0xFFFF;
 					
-					//printf("iSendBufferCounter=%d + sLineLength=%d + SIZE_LENGTH=%d > MAX_CHARS_TCP=%d\n",
+					//printThis("iSendBufferCounter=%d + sLineLength=%d + SIZE_LENGTH=%d > MAX_CHARS_TCP=%d\n",
 					//	iSendBufferCounter, sLineLength, SIZE_LENGTH, MAX_CHARS_TCP);
 					
 					if(iSendBufferCounter + sLineLength + SIZE_LENGTH > MAX_CHARS_TCP)
 					{
-						printf("Sending %d Bytes\n", iSendBufferCounter);
+						printThis("Sending %d Bytes\n", iSendBufferCounter);
 						write(fdd, p_bSendBuffer, iSendBufferCounter);
 						
 						iSendBufferCounter = 0;
@@ -217,7 +285,7 @@ void *tcpRequests(void *)
 			
 			if(iSendBufferCounter > 0)
 			{
-				printf("Sending Last %d Bytes\n", iSendBufferCounter);
+				printThis("Sending Last %d Bytes\n", iSendBufferCounter);
 				write(fdd, p_bSendBuffer, iSendBufferCounter);
 			}
 			
@@ -231,14 +299,14 @@ void *tcpRequests(void *)
 
 				read(fdd, &lenName, 2);
 
-				printf("lenName: %hu %04hx\n", lenName, lenName);
+				printThis("lenName: %hu %04hx\n", lenName, lenName);
 
 				name = (char*)malloc(lenName+1);
 
 				read(fdd, name, lenName);
 				name[lenName] = '\0';
 
-				printf("Name: %s\n", name);
+				printThis("Name: %s\n", name);
 			}
 
 			{
@@ -246,7 +314,7 @@ void *tcpRequests(void *)
 
 				read(fdd, &lenFile, 4);
 
-				printf("Filesize: %u %08x\n", lenFile, lenFile);
+				printThis("Filesize: %u %08x\n", lenFile, lenFile);
 
 				char buffer[MAX_CHARS_TCP]; 
 
@@ -256,7 +324,7 @@ void *tcpRequests(void *)
 				for(int i = 0; i < lenFile;) {
 					short bytesToRead = (lenFile - i)>MAX_CHARS_TCP?MAX_CHARS_TCP:lenFile-i;
 
-					//printf("remaining %d [%d]", lenFile - i, bytesToRead);
+					//printThis("remaining %d [%d]", lenFile - i, bytesToRead);
 
 					read(fdd, &buffer, bytesToRead);
 					fwrite (buffer , 1 , bytesToRead , pFile );
@@ -276,23 +344,206 @@ void *tcpRequests(void *)
 	
 }
 
+#define SOCKETNAME "/tmp/e2control.socket"
+static int vShutdown = 0;
 
 
-int main(void)
+int listenControlSocket(int fdc)
 {
-	pthread_t answerUdpBroadcastThread;
-	pthread_t tcpRequestsThread;
-	int error = 0;
+	dprintf("%s->\n", __FUNCTION__);
+	int fdd;
+	
+
+	if (listen(fdc, 15) < 0) {
+		printThis("listenControlSocket - listen error\n");
+	}
+
+	while(1) {
+		// accept connection
+	dprintf("%s->\n", __FUNCTION__);
+		struct sockaddr_in socketRemote;
+		memset(&socketRemote, 0, sizeof(struct sockaddr_in));
+		socklen_t len = sizeof(struct sockaddr_in);
+		if ((fdd = accept(fdc, (struct sockaddr*)&socketRemote, &len)) < 0) {
+			perror("listenControlSocket - accept error\n");
+			continue;
+		}
+		int cmd = 0;
+		read(fdd, &cmd, sizeof(int));
+		printThis("Command: %02d\n", cmd);
+		switch(cmd)
+		{
+			case CONTROL_CMD_GETLOGFILENAME: write (fdd, filename, strlen(filename)); break;
+			case CONTROL_CMD_SHUTDOWN: vShutdown = 1; break;
+			default:
+				break;
+		}
+
+	dprintf("%s-<\n", __FUNCTION__);
+
+		/*printThis("%s:%d\n", __FUNCTION__, __LINE__);
+		//close(fdc);
+
+		printThis("Got TCP-Connection from %s\n", inet_ntoa(remote.sin_addr));
+
+		//First of read what the client want
+		int slen = read(fdd, string, strlen(MAGIC_REQ_PROTOYPE));*/
+	}
+	dprintf("%s-<\n", __FUNCTION__);
+	return 0;
+}
+
+int sendControlCommand(int cmd, unsigned char * buffer, int * bufferlen)
+{
+	dprintf("%s->\n", __FUNCTION__);
+	struct sockaddr_in serv_addr;
+	int sockfd = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&serv_addr, 0, sizeof(struct sockaddr_in));
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(CONTROL_PORT);
+	serv_addr.sin_addr.s_addr = inet_addr ("127.0.0.1"); 
+	socklen_t len = sizeof(struct sockaddr_in);
+	if(connect(sockfd, (struct sockaddr *) &serv_addr, len) != 0)
+		perror("Connect error\n");
+
+	write(sockfd, &cmd, sizeof(int));
+	if(*bufferlen > 0)
+		*bufferlen = read(sockfd, buffer, *bufferlen);
+	close(sockfd);
+	dprintf("%s-<\n", __FUNCTION__);
+}
+
+int createControlSocket()
+{
+	dprintf("%s->\n", __FUNCTION__);
+	int success = 0;
+
+	struct sockaddr_in socketLocal;
+	int fdControl = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&socketLocal, 0, sizeof(struct sockaddr_in));
+	socketLocal.sin_family = AF_INET;
+	socketLocal.sin_port = htons(CONTROL_PORT);
+	socketLocal.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(fdControl, (struct sockaddr*)&socketLocal, sizeof(struct sockaddr_in)) >= 0)
+		listenControlSocket(fdControl);
+
+	dprintf("%s-<\n", __FUNCTION__);
+	return fdControl;
+}
+
+int checkControlSocket()
+{
+	dprintf("%s->\n", __FUNCTION__);
+	int running = 0;
+
+	struct sockaddr_in socketLocal;
+	int fdControl = socket(PF_INET, SOCK_STREAM, 0);
+	memset(&socketLocal, 0, sizeof(struct sockaddr_in));
+	socketLocal.sin_family = AF_INET;
+	socketLocal.sin_port = htons(CONTROL_PORT);
+	socketLocal.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(fdControl, (struct sockaddr*)&socketLocal, sizeof(struct sockaddr_in)) < 0)
+		running = 1;
+
+	close(fdControl);
+	dprintf("%s-<, %d\n", __FUNCTION__, running);
+	return running;
+
+}
+
+void * control (void * none)
+{
+	int fdControl = createControlSocket();
+	//listenControlSocket(fdControl);
+}
+
+int checkForRunningInstance()
+{
+	return checkControlSocket();
+}
 
 
-	error = pthread_create(&answerUdpBroadcastThread, NULL, answerUdpBroadcast, NULL);
-	if(error) printf("Error while creating answerUdpBroadcast Thread!\n");
-
-	error = pthread_create(&tcpRequestsThread, NULL, tcpRequests, NULL);
-	if(error) printf("Error while creating answerUdpBroadcast Thread!\n");
 
 
-	pthread_exit(NULL);
+
+int startDaemon()
+{
+	dprintf("%s->\n", __FUNCTION__);
+	pid_t pid = fork();
+	if (pid == 0)
+	{
+		openLogfile();
+		printThis("starting daemon (%d)\n", pid);
+		pthread_t answerUdpBroadcastThread;
+		pthread_t tcpRequestsThread;
+		pthread_t controlThread;
+		int error = 0;
+
+		error = pthread_create(&controlThread, NULL, control, NULL);
+		if(error) printThis("Error while creating control Thread!\n");
+
+		error = pthread_create(&answerUdpBroadcastThread, NULL, answerUdpBroadcast, NULL);
+		if(error) printThis("Error while creating answerUdpBroadcast Thread!\n");
+
+		error = pthread_create(&tcpRequestsThread, NULL, tcpRequests, NULL);
+		if(error) printThis("Error while creating tcpRequests Thread!\n");
+
+		//pthread_exit(NULL);
+		while(!vShutdown)
+		{
+			usleep(200000);
+		}
+		closeLogfile();
+	}
+	dprintf("%s-<\n", __FUNCTION__);
+	return 0;
+}
+
+int main(int argc, char**argv)
+{
+	fpLogfile = stdout;
+
+	if(checkForRunningInstance() == 0)
+	{
+		startDaemon();
+	} 
+	else
+	{
+		//dprintf("%s argv: %s\n", __FUNCTION__, argv[1]);
+		if (argc == 2 && !strncmp(argv[1], "log", 3))
+		{
+			int bufSize = 1024;
+			char logfileName[1024];
+			sendControlCommand(CONTROL_CMD_GETLOGFILENAME, logfileName, &bufSize);
+			if(bufSize > 0) {
+				logfileName[bufSize] = '\0';
+				printf("LogfileName: %s\n", logfileName);
+				FILE * fp = fopen(logfileName, "r");
+				if (fp == NULL) perror ("Error opening file");
+				char buf[1024];
+				int lineNumber = 1;
+				while(fgets (buf , 1024 , fp) > 0)
+				{
+					printf ("[%03d] %s", lineNumber++, buf);
+				}
+     				fclose (fp);
+			}
+		}
+		else if (argc == 2 && !strncmp(argv[1], "stop", 4))
+		{
+			int bufSize = 0;
+			sendControlCommand(CONTROL_CMD_SHUTDOWN, NULL, &bufSize);
+		}
+		else
+			printf( "\nProcess is already running!\n"
+				"\n"
+				"Options are:\n"
+				"\tlog         shows log\n"
+				"\tstop        stop daemon\n"
+				"\n");
+	}
 	return 0;
 }
 
