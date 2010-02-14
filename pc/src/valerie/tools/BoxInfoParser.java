1de3330b0c48034484150a2c6a4df3ecb09ccf0b
schischu65 <schischu65@87702144-8a56-11de-a519-b78fc8232508>
2010-02-14 20:16:26 +0000

    [pc] * this hopefully fixes any isues with autosearch, as now all netweork adapters available are used to detect any boxes

--- pc/src/valerie/tools/BoxInfoParser.java
+++ pc/src/valerie/tools/BoxInfoParser.java
@@ -49,8 +49,19 @@ public class BoxInfoParser {
                     }
                 }
             }
-            
-            list.add(boxinfo);
+
+            boolean alreadyIn = false;
+            for(int i = 0; i < list.size(); i++) { //A little bit hacky
+                String a = boxinfo.IpAddress.getHostAddress();
+                String b = ((BoxInfo)list.get(i)).IpAddress.getHostAddress();
+                if (a.equals(b)) {
+                    alreadyIn = true;
+                    break;
+                }
+            }
+
+            if(!alreadyIn)
+                list.add(boxinfo);
         }
         if(list.size()> 0) {
             BoxInfo[] boxinfos = new BoxInfo[list.size()];
