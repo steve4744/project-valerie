985df9cb75c705bcd3e4cec238b31ed65a44a7cf
schischu65 <schischu65@87702144-8a56-11de-a519-b78fc8232508>
2010-02-15 22:05:06 +0000

    * keeping release.bat up to date

--- pc/release.bat
+++ pc/release.bat
@@ -5,12 +5,17 @@ mkdir release\lib
 mkdir release\bin
 mkdir release\converted
 mkdir release\db
+mkdir release\db\episodes
 mkdir release\download
+mkdir release\import
 
 copy dist\Valerie.jar release\
 copy dist\lib release\lib\
 
+copy bin\convert.exe release\bin\
+copy bin\jpeg2yuv.exe release\bin\
 copy bin\mencoder.exe release\bin\
+copy bin\mpeg2enc.exe release\bin\
 copy bin\pngquant.exe release\bin\
 copy valerie.properties release\
 copy replacements.txt release\
