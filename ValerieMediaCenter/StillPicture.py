1dd6d0297b58a806e93cef132d88c91b16dbd93d
schischu65 <schischu65@87702144-8a56-11de-a519-b78fc8232508>
2011-05-04 18:10:04 +0000

    [valerie] fix crash of stillpicture

--- ValerieMediaCenter/StillPicture.py
+++ ValerieMediaCenter/StillPicture.py
@@ -39,10 +39,10 @@ class StillPicture(Renderer, InfoBarBase):
 
 	def addEventTracker(self):
 		printl("", self)
-		if self.session.nav is not None:
+		if self.session is not None and self.session.nav is not None:
 			self.session.nav.event.append(self.event)
 		else:
-			printl("ARGGHHHH!!! self.session.nav is None", self)
+			printl("ARGGHHHH!!! self.session is not None and self.session.nav is not None", self, "E")
 		#self.eventSafe = self.session.nav.event
 		#self.session.nav.event = []
 		#self.session.nav.event.append(self.event)
@@ -51,10 +51,10 @@ class StillPicture(Renderer, InfoBarBase):
 	
 	def removeEventTracker(self):
 		printl("", self)
-		if self.session.nav is not None:
+		if self.session is not None and self.session.nav is not None:
 			self.session.nav.event.remove(self.event)
 		else:
-			printl("ARGGHHHH!!! self.session.nav is None", self)
+			printl("ARGGHHHH!!! self.session is not None and self.session.nav is not None", self, "E")
 		#print "addEventTracker.eventSafe", self.eventSafe
 		#print "addEventTracker.session.nav.event", self.session.nav.event
 		#self.session.nav.event = self.eventSafe
@@ -67,13 +67,13 @@ class StillPicture(Renderer, InfoBarBase):
 
 	def poll(self):
 		#print "poll"
-		if self.session.nav is not None:
+		if self.session is not None and self.session.nav is not None:
 			service = self.session.nav.getCurrentService()
 			seek = service and service.seek()
 			if seek is not None:
 				pos = seek.getPlayPosition()
 		else:
-			printl("ARGGHHHH!!! self.session.nav is None", self)
+			printl("ARGGHHHH!!! self.session is not None and self.session.nav is not None", self, "E")
 
 	def pollStart(self):
 		printl("", self)
@@ -87,10 +87,10 @@ class StillPicture(Renderer, InfoBarBase):
 
 	def __evEOF(self):
 		printl("", self)
-		if self.session.nav is not None:
+		if self.session is not None and self.session.nav is not None:
 			self.session.nav.playService(eServiceReference(4097, 0, self.getStillpicture()), forceRestart=True)
 		else:
-			printl("ARGGHHHH!!! self.session.nav is None", self)
+			printl("ARGGHHHH!!! self.session is not None and self.session.nav is not None", self, "E")
 		#self.showStillPicture()
 
 	onClose = []
@@ -137,13 +137,13 @@ class StillPicture(Renderer, InfoBarBase):
 				if self.isLoop is False:
 					self.showiframe.showStillpicture(self.getStillpicture())
 				elif self.isLoop is True:
-					if self.session.nav is not None:
+					if self.session is not None and self.session.nav is not None:
 						print "showStillPicture", "loop", self.getStillpicture()
 						ServiceEventTracker.setActiveInfoBar(self, None, None)
 						self.session.nav.playService(eServiceReference(4097, 0, self.getStillpicture()))
 						self.pollStart()
 					else:
-						printl("ARGGHHHH!!! self.session.nav is None", self)
+						printl("ARGGHHHH!!! self.session is not None and self.session.nav is not None", self, "E")
 			except Exception, ex:
 				print ex
 		#print "showStillPicture", "<--"
@@ -155,11 +155,11 @@ class StillPicture(Renderer, InfoBarBase):
 					self.showiframe.finishStillPicture()
 				elif self.isLoop is True:
 					self.pollStop()
-					if self.session.nav is not None:
+					if self.session is not None and self.session.nav is not None:
 						self.session.nav.stopService()
 						ServiceEventTracker.popActiveInfoBar()
 					else:
-						printl("ARGGHHHH!!! self.session.nav is None", self)
+						printl("ARGGHHHH!!! self.session is not None and self.session.nav is None", self, "E")
 			except Exception, ex:
 				print ex
 
