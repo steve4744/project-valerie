4f080568fdc16073f800369e65328cd1874cc972
schischu65@googlemail.com <schischu65@googlemail.com@87702144-8a56-11de-a519-b78fc8232508>
2011-06-03 11:46:24 +0000

    [valerie] Adding plugin dreamnetcast

---
+++ ValerieMediaCenter/DMC_Plugins/DMC_DreamNetcast.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from Plugins.Extensions.ProjectValerie.__common__ import printl2 as printl
+from Plugins.Extensions.ProjectValerie.__plugin__ import Plugin, registerPlugin
+
+gAvailable = False
+try:
+	from geminidreamnetcast import geminidreamnetcast
+	gAvailable = True
+except Exception, ex:
+	printl("Exception(" + str(type(ex)) + "): " + str(ex), __name__, "E")
+	gAvailable = False
+
+if gAvailable is True:
+	registerPlugin(Plugin(name=_("Dreamnetcast"), start=geminidreamnetcast, where=Plugin.MENU_MUSIC))
