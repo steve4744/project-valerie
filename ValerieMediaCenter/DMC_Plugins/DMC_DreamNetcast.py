1703ea88e8043f28399ed03a0dbdcadea323128e
schischu65@googlemail.com <schischu65@googlemail.com@87702144-8a56-11de-a519-b78fc8232508>
2011-06-03 15:02:32 +0000

    [valerie] dreamnetcast fix?

--- ValerieMediaCenter/DMC_Plugins/DMC_DreamNetcast.py
+++ ValerieMediaCenter/DMC_Plugins/DMC_DreamNetcast.py
@@ -5,7 +5,10 @@ from Plugins.Extensions.ProjectValerie.__plugin__ import Plugin, registerPlugin
 
 gAvailable = False
 try:
-	from geminidreamnetcast import geminidreamnetcast
+	try:
+		from Plugins.Bp.geminidreamnetcast.geminidreamnetcast import geminidreamnetcast
+	except:
+		from Plugins.Bp.geminidreamnetcast import geminidreamnetcast
 	gAvailable = True
 except Exception, ex:
 	printl("Exception(" + str(type(ex)) + "): " + str(ex), __name__, "E")
