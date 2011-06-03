47e0576021126a141d0a7c36e93787f385b38d90
schischu65@googlemail.com <schischu65@googlemail.com@87702144-8a56-11de-a519-b78fc8232508>
2011-06-03 09:12:05 +0000

    [valerie] force displaying of sync

--- ValerieMediaCenter/DMC_Plugins/DMC_ProjectValerieSync.py
+++ ValerieMediaCenter/DMC_Plugins/DMC_ProjectValerieSync.py
@@ -10,6 +10,7 @@ from Plugins.Extensions.ProjectValerie.__plugin__ import Plugin, registerPlugin
 #------------------------------------------------------------------------------------------
 
 gAvailable = False
+gLateImport = False
 try:
 	try:
 		from Plugins.Extensions.ProjectValerieSync.plugin import ProjectValerieSync as PluginProjectValerieSync
@@ -25,23 +26,44 @@ except Exception, ex:
 config.plugins.pvmc.plugins.sync = ConfigSubsection()
 config.plugins.pvmc.plugins.sync.fastsynconautostart = ConfigYesNo(default=False)
 
-class ProjectValerieSync(PluginProjectValerieSync):
-
-	#If custom skin should be used, define it here or in the skin.xml file
-	#skin = "<screen ...."
-
-	def __init__(self, session):
-		PluginProjectValerieSync.__init__(self, session)
-		# If no own screen os provided, use the one of the plugin
-		self.skinName = "ProjectValerieSync"
+try:
+	class ProjectValerieSync(PluginProjectValerieSync):
+	
+		#If custom skin should be used, define it here or in the skin.xml file
+		#skin = "<screen ...."
+	
+		def __init__(self, session):
+			PluginProjectValerieSync.__init__(self, session)
+			# If no own screen os provided, use the one of the plugin
+			self.skinName = "ProjectValerieSync"
+except Exception, ex:
+	printl("Exception(" + str(type(ex)) + "): " + str(ex), __name__, "E")
 
 def settings():
 	s = []
 	s.append((_("Fast Sync on autostart"), config.plugins.pvmc.plugins.sync.fastsynconautostart, ))
 	return s
 
+def autostartPlugin(session):
+	from Plugins.Extensions.ProjectValerieSync.plugin import autostart
+	autostart(session)
+
+def startPlugin(session):
+	from Plugins.Extensions.ProjectValerieSync.plugin import ProjectValerieSync as PluginProjectValerieSync
+	session.open(PluginProjectValerieSync)
+
+if gAvailable is False:
+	printl("WORKAROUND: Force displaying of this plugin!", __name__, "W")
+	gAvailable = True
+	gLateImport = True
+
 if gAvailable is True:
 	registerPlugin(Plugin(name=_("Syncronize"), fnc=settings, where=Plugin.SETTINGS))
-	registerPlugin(Plugin(name=_("Syncronize"), start=ProjectValerieSync, where=Plugin.MENU_SYSTEM, supportStillPicture=True))
-	if config.plugins.pvmc.plugins.sync.fastsynconautostart.value is True:
-		registerPlugin(Plugin(name=_("Syncronize"), fnc=autostart, where=Plugin.AUTOSTART))
+	if gLateImport is True:
+		registerPlugin(Plugin(name=_("Syncronize"), fnc=startPlugin, where=Plugin.MENU_SYSTEM, supportStillPicture=True))
+		if config.plugins.pvmc.plugins.sync.fastsynconautostart.value is True:
+			registerPlugin(Plugin(name=_("Syncronize"), fnc=autostartPlugin, where=Plugin.AUTOSTART))
+	else:
+		registerPlugin(Plugin(name=_("Syncronize"), start=ProjectValerieSync, where=Plugin.MENU_SYSTEM, supportStillPicture=True))
+		if config.plugins.pvmc.plugins.sync.fastsynconautostart.value is True:
+			registerPlugin(Plugin(name=_("Syncronize"), fnc=autostart, where=Plugin.AUTOSTART))
