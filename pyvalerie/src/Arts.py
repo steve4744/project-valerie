c7c3ce9bf639b55bfbdf3835a02194e358156a2c
schischu65@googlemail.com <schischu65@googlemail.com@87702144-8a56-11de-a519-b78fc8232508>
2011-07-18 10:44:52 +0000

    [pysync] fix crash in arts.py

--- pyvalerie/src/Arts.py
+++ pyvalerie/src/Arts.py
@@ -59,14 +59,14 @@ class Arts():
 			if len(eInfo.Poster):
 				isMissing = False
 				if path.isfile(WebGrabber.downloadDir + "/" + eInfo.ImdbId + "_poster_" + self.posterResolution[0] + ".png") is False:
-					if eInfo.Poster[0] == "/":
+					if eInfo.Poster[0] != "/":
 						self.save(self.URL + self.CONVERT + eInfo.ImdbId + ";poster;" + eInfo.Poster)
 					else:
 						self.save(self.URL + self.CONVERT2USER + "?id=" + eInfo.ImdbId + "&type=poster&user=true", eInfo.Poster)
 			
 			if len(eInfo.Backdrop):
 				if path.isfile(WebGrabber.downloadDir + "/" + eInfo.ImdbId + "_backdrop.m1v") is False:
-					if eInfo.Poster[0] == "/":
+					if eInfo.Poster[0] != "/":
 						self.save(self.URL + self.CONVERT + eInfo.ImdbId + ";backdrop;" + eInfo.Backdrop)
 					else:
 						self.save(self.URL + self.CONVERT2USER + "?id=" + eInfo.ImdbId + "&type=backdrop&user=true", eInfo.Backdrop)
@@ -74,14 +74,14 @@ class Arts():
 		elif eInfo.isTypeSerie() or eInfo.isTypeEpisode():
 			if len(eInfo.Poster):
 				if path.isfile(WebGrabber.downloadDir + "/" + eInfo.TheTvDbId + "_poster_" + self.posterResolution[0] + ".png") is False:
-					if eInfo.Poster[0] == "/":
+					if eInfo.Poster[0] != "/":
 						self.save(self.URL + self.CONVERT + eInfo.TheTvDbId + ";poster;" + eInfo.Poster)
 					else:
 						self.save(self.URL + self.CONVERT2USER + "?id=" + eInfo.TheTvDbId + "&type=poster&user=true", eInfo.Poster)
 			
 			if len(eInfo.Backdrop):
 				if path.isfile(WebGrabber.downloadDir + "/" + eInfo.TheTvDbId + "_backdrop.m1v") is False:
-					if eInfo.Poster[0] == "/":
+					if eInfo.Poster[0] != "/":
 						self.save(self.URL + self.CONVERT + eInfo.TheTvDbId + ";backdrop;" + eInfo.Backdrop)
 					else:
 						self.save(self.URL + self.CONVERT2USER + "?id=" + eInfo.TheTvDbId + "&type=backdrop&user=true", eInfo.Backdrop)
